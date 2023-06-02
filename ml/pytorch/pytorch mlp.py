e4e570a8230b00caf2296e8977ce016f59c63ee1
Patrick Pragman <patrickpragman@Patricks-iMac.local>
2023-06-01 17:23:59 -0800

    more pytorch tinkering

--- ml/pytorch/pytorch mlp.py
+++ ml/pytorch/pytorch mlp.py
@@ -92,8 +92,8 @@ if __name__ == "__main__":
         size = len(dataset)
         model.train()
         for batch, (X, y) in enumerate(dataset):
-            X.type(torch.LongTensor)
-            y.type(torch.LongTensor)
+            X = X.type(torch.LongTensor)
+            y = y.type(torch.LongTensor)
             X, y = X.to(device), y.to(device)
 
             # Compute prediction error
@@ -117,8 +117,8 @@ if __name__ == "__main__":
         test_loss, correct = 0, 0
         with torch.no_grad():
             for X, y in dataset:
-                X.type(torch.LongTensor)
-                y.type(torch.LongTensor)  # cast to GPU appropriate type
+                X = X.type(torch.LongTensor)
+                y = y.type(torch.LongTensor)  # cast to GPU appropriate type
 
                 X, y = X.to(device), y.to(device)
                 pred = model(X)
