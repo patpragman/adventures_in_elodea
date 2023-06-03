d4b132055d84df03d945833d67b95e18cf536379
Patrick Pragman <patrickpragman@Patricks-iMac.local>
2023-06-02 17:54:42 -0800

    training reading tweak

--- ml/pytorch/pytorch mlp.py
+++ ml/pytorch/pytorch mlp.py
@@ -3,6 +3,7 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import transforms
 from dataset import ElodeaImages
+from sklearn.metrics import confusion_matrix, classification_report
 
 # some hyperparams
 BATCH_SIZE = 32
@@ -111,19 +112,23 @@ if __name__ == "__main__":
 
 
     def test(dataset, model, loss_fn):
+
+
         size = len(dataset)
-        num_batches = len(dataset)
+        num_batches = 0
         model.eval()
         test_loss, correct = 0, 0
+
         with torch.no_grad():
             for X, y in dataset:
-                #X = X.type(torch.LongTensor)
                 y = y.type(torch.LongTensor)  # cast to GPU appropriate type
-
                 X, y = X.to(device), y.to(device)
+
                 pred = model(X)
                 test_loss += loss_fn(pred, y).item()
                 correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+                size += 1
+
         test_loss /= num_batches
         correct /= size
         print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
