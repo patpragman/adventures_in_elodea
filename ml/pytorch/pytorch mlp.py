55a518959b66c4d923f21454c17edcc2a136b5d9
Patrick Pragman <patrickpragman@Patricks-iMac.local>
2023-06-03 08:28:14 -0800

    learning about CPU/GPU in HPC

--- ml/pytorch/pytorch mlp.py
+++ ml/pytorch/pytorch mlp.py
@@ -134,16 +134,18 @@ if __name__ == "__main__":
                 correct += (pred.argmax(1) == y).type(torch.float).sum().item()
                 size += 1
 
-        y_preds = np.array([model(X.to(device)) for X, _ in dataset])
-        y_true = np.array([y.type(torch.LongTensor) for _, y in dataset])
+            y_preds = np.array([model(X.to(device)) for X, _ in dataset])
+            y_true = np.array([y.type(torch.LongTensor) for _, y in dataset])
+
+            cm = confusion_matrix(y_true, y_preds)
+            r = classification_report(y_true, y_preds)
 
 
         test_loss /= num_batches
         correct /= size
         print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
-        cm = confusion_matrix(y_true, y_preds)
-        r = classification_report(y_true, y_preds)
+
 
         print(f'Confusion Matrix:  \n{cm}')
         print(r)
